667a0fa9afaac11d7df418f2800a1ffc559cbded
Иван Кисляков <hodgivan@gmail.com>
2024-03-22 14:31:17 +0500

    For portfolio

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,8 +14,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1',
-                 '158.160.5.135', 'foodgramproject.onthewifi.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 # Application definition
 
